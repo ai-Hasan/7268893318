838926850d2695a7937258ce95f47fc78f1b8264
Al Hasan <alhasan123pbh@gmail.com>
2024-10-11 20:11:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,90 @@
+<!DOCTYPE html><html lang=en><head><meta charset=utf-8><meta http-equiv=X-UA-Compatible content="IE=edge"><meta name=viewport content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no,viewport-fit=cover"><meta name=apple-mobile-web-app-capable content=yes><link rel=icon href=icon.png><link rel=manifest href="/manifest.json?v=1"><meta name=theme-color content=#ffffff><meta name=apple-mobile-web-app-capable content=no><meta name=apple-mobile-web-app-status-bar-style content=default><meta name=apple-mobile-web-app-title content=VClub><meta property=url content=https://www.vclub.in><meta property=type content=article><meta property=title content="vclub.in invite You To Easy Earn Anytime Anywhere"><meta property=description content="vclub india Local Platform☑Free Bonus ₹121☑₹1,00,00,000 Agent Bonus☑10 Mn+ Players ☑100% Safe&Easy."><meta property=image ccontent=v-logo.png><meta property=site_name content="The Only One Earning Platform"><meta property=author content="The Only One Earning Platform"><meta property=fb:app_id content=2051727658305254><meta property=og:url content=https://www.vclub.in><meta property=og:type content=article><meta property=og:title content="vclub.in invite You To Easy Earn Anytime Anywhere"><meta property=og:description content="vclub india Local Platform☑Free Bonus ₹121☑₹1,00,00,000 Agent Bonus☑10 Mn+ Players ☑100% Safe&Easy."><meta property=og:image content=v-logo.png><meta property=og:site_name content="The Only One Earning Platform"><meta property=og:author content="The Only One Earning Platform"><meta property=twitter:url content=https://www.vclub.in><meta property=twitter:type content=article><meta property=twitter:title content="vclub.in invite You To Easy Earn Anytime Anywhere"><meta property=twitter:description content="vclub india Local Platform☑Free Bonus ₹121☑₹1,00,00,000 Agent Bonus☑10 Mn+ Players ☑100% Safe&Easy."><meta property=twitter:image content=v-logo.png><meta name=twitter:image:src content=v-logo.png><meta property=twitter:site_name content="The Only One Earning Platform"><meta property=twitter:author content="The Only One Earning Platform"><meta property=al:url content=https://www.vclub.in><meta property=al:type content=article><meta property=al:title content="vclub.in invite You To Easy Earn Anytime Anywhere"><meta property=al:description content="vclub india Local Platform☑Free Bonus ₹121☑₹1,00,00,000 Agent Bonus☑10 Mn+ Players ☑100% Safe&Easy."><meta property=al:image content=v-logo.png><meta property=al:site_name content="The Only One Earning Platform"><meta property=al:author content="The Only One Earning Platform"><title>vclub.in invite You To Easy Earn Anytime Anywhere</title><script src=./static/js/fbpixels.js></script><script src=https://cdnjs.cloudflare.com/ajax/libs/fingerprintjs2/2.1.5/fingerprint2.min.js></script><style>.progress-container-box {
+      position: fixed;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100vh;
+      background: #fff;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+    }
+
+    /* 进度条容器样式 */
+    .progress-container {
+      width: 80%;
+      background-color: #f1f1f1;
+      border-radius: 15px;
+      overflow: hidden;
+    }
+
+    /* 进度条样式 */
+    .progress-bar {
+      width: 0;
+      height: 30px;
+      background-color: #4CAF50;
+      text-align: center;
+      line-height: 30px;
+      color: white;
+    }</style><link href=css/app.87a06978.css rel=preload as=style><link href=css/chunk-vendors.235b8733.css rel=preload as=style><link href=js/app.1fac1cea.js rel=preload as=script><link href=js/chunk-vendors.15d35795.js rel=preload as=script><link href=css/chunk-vendors.235b8733.css rel=stylesheet><link href=css/app.87a06978.css rel=stylesheet></head><body><noscript><strong>vclub india Local Platform☑Free Bonus ₹121☑₹1,00,00,000 Agent Bonus☑10 Mn+ Players ☑100% Safe&Easy.</strong></noscript><div id=app style="overflow:auto;overflow-x: hidden"></div><script>// (function () {
+
+    //   // 获取进度条元素
+    //   var progressBar = document.getElementById("progress");
+    //   progressBar.style.display = 'flex';
+
+    //   // 设置总时间（30秒）
+    //   var totalTime = 30;
+    //   // 设置更新频率（每100毫秒更新一次）
+    //   var updateInterval = 32;
+
+    //   // 计算每次更新的进度值
+    //   var increment = (100 / totalTime) * (updateInterval / 1000);
+
+    //   var currentProgress = 30;
+
+    //   function updateProgressBar() {
+    //     currentProgress += increment;
+    //     progressBar.style.width = currentProgress + "%";
+
+    //     if (currentProgress < 100) {
+    //       // 继续更新进度条直到完成
+    //       setTimeout(updateProgressBar, updateInterval);
+    //     }
+    //   }
+
+    //   // 启动进度条更新
+    //   updateProgressBar();
+    // })()</script><script src=js/chunk-vendors.15d35795.js></script><script src=js/app.1fac1cea.js></script></body><script>if (navigator.serviceWorker != null) {
+    navigator.serviceWorker.register('./service-worker.js')
+      .then(function (registration) {
+        console.log('Registered events at scope: ', registration.scope);
+      });
+  }</script><script>mui.init({
+    keyEventBind: {
+      backbutton: true,
+    }
+  })
+  var first = null;
+  mui.back = function () {
+    if (!first) {
+      first = new Date().getTime();
+      var route_name = window.location.hash;
+      if (route_name.search('mine') != -1 || route_name.search('login') != -1) {
+        mui.toast("Press again to exit the app");
+        setTimeout(function () {
+          first = null;
+        }, 500);
+      } else {
+        history.go(-1);
+        first = null;
+      };
+
+
+    } else {
+      if (new Date().getTime() - first < 500) {
+        plus.runtime.quit();
+      }
+    }
+  
+  </script></html>
